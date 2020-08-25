a157fd57eb0b8762f51b5927bd5be54a0f330bce
AdityaPusalkar <aditya.pusalkar@iitgn.ac.in>
2020-08-26 00:05:19 +0530

    First File Added

---
+++ a.c
@@ -0,0 +1,14 @@
+#include<stdio.h> 
+#include<string.h> 
+#include<stdlib.h> 
+#include<unistd.h> 
+#include<sys/types.h> 
+#include<sys/wait.h> 
+#include<readline/readline.h> 
+#include<readline/history.h> 
+
+int main()
+{
+    printf("Welcome to my Shell!");
+    return 0;
+}
