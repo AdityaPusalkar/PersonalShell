98ac1a685a8c8e16b09c93e5e92eb07325977b3f
AdityaPusalkar <aditya.pusalkar@iitgn.ac.in>
2020-08-27 00:08:00 +0530

    Input and Parsing added

--- a.c
+++ a.c
@@ -5,26 +5,85 @@
 #include<sys/types.h> 
 #include<sys/wait.h> 
 
-void shell_input()
+char **shell_parse(char *input)
 {
+  int buflen = 48;
+  int len = buflen;
+  int index = 0;
+  char **args = malloc(len * sizeof(char*));
+  char *arg;
+  if (!args) {
+    fprintf(stderr, "lsh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+  char *delim = " \t\r\n\a";
+  arg = strtok(input,delim);
+  while(arg!=NULL)
+  {
+    args[index] = arg;
+    index++;
+    if(index>=len)
+    {
+      len+=buflen;
+      args = realloc(args, len * sizeof(char*));
+      if(!args)
+      {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+    arg = strtok(NULL, delim);
+  }
+  args[index] = NULL;
+  return args;
+}
 
+char *shell_input()
+{
+  char *input = NULL;
+  ssize_t bufsize = 0;
+  if (getline(&input, &bufsize, stdin) == -1)
+  {
+    if (feof(stdin)) 
+    {
+      exit(EXIT_SUCCESS);
+    } 
+    else  
+    {
+      perror("readline");
+      exit(EXIT_FAILURE);
+    }
+  }
+  return input;
 }
 void shell_loop()
 {
     char *input;
-    int nums = 10;
+    char **args;
+    int n = 10;
     do
     {
         printf("--> ");
         input = shell_input();
-        free(input);
+        printf("You entered: %s",input);
+        args = shell_parse(input);
+        int index = 0;
+        printf("Space separated: \n");
+        while(args[index]!=NULL)
+        {
+          printf("%d: %s\n",(index+1),args[index]);
+          index++;
+        }
+        printf("\n");
         n--;
-    } while(n);
+        free(input);
+        free(args);
+    } while(n>0);
 }
 
 int main(int argc, char **argv)
 {
-    printf("Welcome to my Shell!");
-    shell_input();
+    printf("Welcome to my Shell!\n");
+    shell_loop();
     return EXIT_SUCCESS;
 }
